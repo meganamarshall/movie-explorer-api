1abb74860122ad065e1d0b1bad32846f3ebca29b
Leslie Shea <leslieyshea@gmail.com>
2019-03-06 16:58:09 -0800

    started test - now switching to movie api

---
+++ test/make-search-artist.test.js
@@ -0,0 +1,15 @@
+const test = QUnit.test;
+
+QUnit.module('making url based on search');
+
+test('url includes query and page', assert => {
+    //arrange
+    const queryOptions = {
+        searchInput: 'michael jackson',
+        page: 1
+    };
+
+    //act
+
+    //assert
+});
