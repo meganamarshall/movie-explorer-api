2c0b3a19c2e03d312f71c8a6222338be2d8629ed
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-03-08 14:05:07 -0800

    refactored make url functions with setting default query params

--- src/movie-api.js
+++ src/movie-api.js
@@ -3,6 +3,11 @@ const SEARCH_MOVIE_URL = `${BASE_URL}/search/movie`;
 const MOVIE_DETAIL_URL = `${BASE_URL}/movie`;
 const API_KEY = 'cb74bb60617505504abd12bd45490b45';
 
+function setDefaultQueryParams(url) {
+    url.searchParams.set('api_key', API_KEY);
+    url.searchParams.set('language', 'en-us');
+}
+
 export function makeSearchMovieUrl(queryOptions) {
     const searchInput = queryOptions.searchInput;
     const yearInput = queryOptions.year;
@@ -10,8 +15,7 @@ export function makeSearchMovieUrl(queryOptions) {
         return '';
     }
     const url = new URL(SEARCH_MOVIE_URL);
-    url.searchParams.set('api_key', API_KEY);
-    url.searchParams.set('language', 'en-us');
+    setDefaultQueryParams(url);
     url.searchParams.set('include_adult', false);
     url.searchParams.set('query', searchInput);
     url.searchParams.set('page', queryOptions.page);
@@ -22,7 +26,6 @@ export function makeSearchMovieUrl(queryOptions) {
 export function makeMovieDetailUrl(movieId) {
     const path = `${MOVIE_DETAIL_URL}/${movieId}`;
     const url = new URL(path);
-    url.searchParams.set('api_key', API_KEY);
-    url.searchParams.set('language', 'en-us');
+    setDefaultQueryParams(url);
     return url.toString();
 }
